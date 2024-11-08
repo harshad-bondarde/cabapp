2a96a004c61bff90db1abaa5226f2f833c7502cf
Harshad <harshadbondarde2206@gmail.com>
2024-11-08 14:57:29 +0530

    updated BookRidesInputBox

--- frontend/src/components/home/BookaRide/HomeBookRIdesInputBox.jsx
+++ frontend/src/components/home/BookaRide/HomeBookRIdesInputBox.jsx
@@ -7,11 +7,7 @@ export function BookRIdesInputBox({label,placeholder,OnChange}){
                 {label} :
             </div>
 
-            {placeholder?
-                <input type="text" onChange={OnChange} placeholder={placeholder} className="border-2 shadow-md rounded-2xl p-2 transition ease-in-out duration-300 hover:-translate-y-1 hover:shadow-lg cursor-pointer  h-14 w-56 "/>
-                :
-                <input type="text" onChange={OnChange} placeholder={label} className="border-2 shadow-md rounded-2xl p-2 transition ease-in-out duration-300 hover:-translate-y-1 hover:shadow-lg cursor-pointer  h-14 w-56"/>
-            }
+            <input type="text" onChange={OnChange} placeholder={placeholder ? placeholder : label} className="border-2 shadow-md rounded-2xl p-2 transition ease-in-out duration-300 hover:-translate-y-1 hover:shadow-lg cursor-pointer  h-14 w-56 "/>
             
         </div>
     )
