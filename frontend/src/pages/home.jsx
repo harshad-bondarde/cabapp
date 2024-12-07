7bd554b67e593fe3ce316eaeb90d602763753b71
Harshad <harshadbondarde2206@gmail.com>
2024-12-07 10:29:39 +0530

    added MapBox-Map over Cabapp/home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -7,9 +7,8 @@ import { AddRide } from "../components/home/HomeAddRide"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { useSearchParams } from "react-router-dom"
-
-
-
+import { useDispatch , useSelector } from 'react-redux'
+import GetRealTimeLocations from "../components/mapboxAPI/getRealTimeLocations"
 
 export function Home() {
 
@@ -17,6 +16,9 @@ export function Home() {
     let [userInfo, setUserInfo] = useState({})
     const [searchParams] = useSearchParams()
 
+    const { showMap , coordinates }=useSelector(state=>state.map)
+    console.log(showMap)
+    console.log(coordinates)
     useEffect(()=>{
         const getinfo=async ()=>{
             try{
@@ -35,13 +37,22 @@ export function Home() {
     },[])
 
     return (
-        <div className="bg-slate-100">
+        <div className="relative bg-slate-100 h-screen">
+            {   showMap &&
+                <div className="absolute z-50 inset-0 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0">
+                    <div className="flex justify-center">
+                        <div className="flex flex-col items-center w-full h-full ml-20 mr-20 ">
+                            <GetRealTimeLocations INITIAL_ZOOM={5}/>
+                        </div>
+                    </div>
+                </div>
+            }
 
             <Header setRenderButton={setRenderButton} firstname={userInfo.firstname} />
             
-            <div className="flex">
+            <div className="flex ">
 
-                <div className="bg-slate-100 h-screen mx-1 ml-2  w-48 mt-2 rounded space-y-4 text-sm shadow-md border-slate-200  ">
+                <div className="bg-slate-100 mx-1 ml-2 h-screen w-48 mt-2 rounded space-y-4 text-sm shadow-md border-slate-200  ">
                     <NavButton label="Book a Ride" renderNumber={1} setRenderButton={setRenderButton} />
                     <NavButton label="Your Bookings" renderNumber={2} setRenderButton={setRenderButton} />
                     <NavButton label="Your Profile" renderNumber={3} setRenderButton={setRenderButton} />
@@ -49,14 +60,26 @@ export function Home() {
 
                 </div>
                 
-                <div className="bg-slate-100 w-full mt-2 mr-2 ml-1 rounded-lg shadow-md ">
+                {/* {   !showMap?
+                    <div className="bg-slate-100  w-full  mt-2 mr-2 ml-1 rounded-lg shadow-md ">
+                        {renderButton == 1 ? <BookRides /> : null}
+                        {renderButton == 2 ? <YourBookings /> : null}
+                        {renderButton == 3 ? <YourProfile userInfo={userInfo} /> : null}
+                        {renderButton == 4 ? <AddRide /> : null}
+                    </div>
+                    :
+                    <GetRealTimeLocations INITIAL_ZOOM={5}/>
+                } */}
+                   
+                <div className="bg-slate-100  w-full  mt-2 mr-2 ml-1 rounded-lg shadow-md ">
                     {renderButton == 1 ? <BookRides /> : null}
                     {renderButton == 2 ? <YourBookings /> : null}
                     {renderButton == 3 ? <YourProfile userInfo={userInfo} /> : null}
                     {renderButton == 4 ? <AddRide /> : null}
                 </div>
-
+                
             </div>
+
         </div>
     )
 }
