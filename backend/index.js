1cf9fe3427639372940db44b7de2c6af39c18576
Harshad <harshadbondarde2206@gmail.com>
2025-01-04 23:54:51 +0530

    fixed bug

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,20 @@ const app=express();
 const PORT=3000
 const mainRouter=require("./routes/index")
 const axios=require("axios")
-
+const path=require("path")
 const cors=require("cors")
+const dir=path.resolve()
+
 app.use(cors())
 app.use(express.json());
 app.use("/",mainRouter)
 
+
 if(process.env.NODE_ENV=='production'){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+  app.use(express.static(path.join(dir,"/frontend/dist")))
 
   app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+      res.sendFile(path.resolve(dir,"frontend","dist","index.html"))
   })
 }
 
