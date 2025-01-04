688ac35317c0fc5a29edd5d1ca009f9e9feaa580
Harshad <harshadbondarde2206@gmail.com>
2025-01-05 00:59:24 +0530

    fixed bug

--- frontend/src/assets/url.jsx
+++ frontend/src/assets/url.jsx
@@ -1,2 +1,2 @@
-export const url=import.meta.env.MODE=='development' ? "http://localhost:3000" : "/"
+export const url=import.meta.env.MODE=='development' ? "http://localhost:3000" : ""
 // export const url="http://localhost:3000"
